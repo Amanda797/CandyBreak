48d1d9b2d86844ab8f1e5bdaa5f75c554cb30997
Amanda797 <agrose071479@live.com>
2022-06-04 12:11:28 -0400

    Coded the win
    Coded it so you can win level if you have broken all the blocks

--- Candy Break/Assets/Scripts/Level.cs
+++ Candy Break/Assets/Scripts/Level.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class Level : MonoBehaviour
 {
+    //paramaters
+
     //Serialized for debugging
     [SerializeField] int breakableBlocks;
+
+    //cached reference
+    SceneLoader sceneloader;
+
+    private void Start()
+    {
+        sceneloader = FindObjectOfType<SceneLoader>();
+    }
 
     public void CountBreakableBlocks()
     {
         breakableBlocks++;
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneloader.LoadNextScene();
+        }
     }
     
 }
