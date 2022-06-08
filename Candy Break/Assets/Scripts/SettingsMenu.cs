ac54d784905938a1210b1def6cd81f35c51d7132
Amanda797 <agrose071479@live.com>
2022-06-07 22:48:44 -0400

    Finished settings menu

--- Candy Break/Assets/Scripts/SettingsMenu.cs
+++ Candy Break/Assets/Scripts/SettingsMenu.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioMixer mainMixer;
+
+    public void SetVolume(float volume)
     {
-        
+        mainMixer.SetFloat("volume", volume);
+    }
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetQuality(int qualityIndex)
     {
-        
+        QualitySettings.SetQualityLevel(qualityIndex);
     }
 }
